108dc9743f08e4b4c238984a5a34bc3a5b316ac9
fajarfadillahh <fajaragustian979@gmail.com>
2023-01-18 21:51:39 +0700

    responsive NotFound.jsx

--- src/parts/NotFound/ErrorMessage.jsx
+++ src/parts/NotFound/ErrorMessage.jsx
@@ -7,13 +7,15 @@ export default function ErrorMessage() {
     <div className="error flex h-screen w-full items-center justify-center">
       <div className="error__container container">
         <div className="error__data text-center">
-          <h1 className="error__title section-title text-[36px]">
+          <h1 className="error__title section-title text-[36px] sm:text-[42px] lg:text-[56px]">
             404 Not Found!
           </h1>
           <p className="error__text section-text">
             The page you're looking for was not found
           </p>
-          <Button url="/">Back to Home</Button>
+          <Button url="/" className="sm:inline-flex">
+            Back to Home
+          </Button>
         </div>
       </div>
     </div>
