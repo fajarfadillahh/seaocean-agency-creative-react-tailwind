88faa3d2d68d20c94997af70713fbd6478fe10a4
fajarfadillahh <fajaragustian979@gmail.com>
2023-01-18 20:59:33 +0700

    create NotFound.jsx

---
+++ src/parts/NotFound/ErrorMessage.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+import Button from "../../components/Button";
+
+export default function ErrorMessage() {
+  return (
+    <div className="error flex h-screen w-full items-center justify-center">
+      <div className="error__container container">
+        <div className="error__data text-center">
+          <h1 className="error__title section-title text-[36px]">
+            404 Not Found!
+          </h1>
+          <p className="error__text section-text">
+            The page you're looking for was not found
+          </p>
+          <Button url="/">Back to Home</Button>
+        </div>
+      </div>
+    </div>
+  );
+}
