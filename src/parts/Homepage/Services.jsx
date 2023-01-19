a6f39c62fc6f78c333b2254c0be5dd79a6b0d089
fajarfadillahh <fajaragustian979@gmail.com>
2023-01-19 23:33:47 +0700

    dark mode & modified Services

--- src/parts/Homepage/Services.jsx
+++ src/parts/Homepage/Services.jsx
@@ -1,13 +1,9 @@
 import React from "react";
 
-import ServicesIcon1 from "../../assets/images/service-icon-(1).svg";
-import ServicesIcon2 from "../../assets/images/service-icon-(2).svg";
-import ServicesIcon3 from "../../assets/images/service-icon-(3).svg";
-
 export default function Services() {
   return (
-    <section className="services section">
-      <div className="services__wrapper bg-[#f9fafc] py-16">
+    <section className="services section transition-all duration-300 dark:bg-gray-900">
+      <div className="services__wrapper bg-[#f9fafc] py-16 transition-all duration-300 dark:bg-gray-800">
         <div className="services__container container grid gap-12">
           <div className="services__data justify-self-center text-center xs:max-w-sm sm:max-w-md lg:max-w-xl">
             <h1 className="services__title section-title">
@@ -22,14 +18,12 @@ export default function Services() {
 
           <div className="services__group grid items-center gap-8 justify-self-center xs:max-w-sm sm:max-w-md lg:max-w-3xl lg:grid-cols-2 xl:max-w-5xl xl:grid-cols-3">
             <div className="services__content flex items-start gap-6">
-              <img
-                src={ServicesIcon1}
-                alt="service icon"
-                className="services__content-icon w-10"
-              />
+              <h3 className="services__content-number font-serif text-[28px] text-gray-900 dark:text-white">
+                01
+              </h3>
 
               <div>
-                <h3 className="services__content-title pb-4 font-serif text-[20px] text-gray-900">
+                <h3 className="services__content-title pb-4 font-serif text-[20px] text-gray-900 dark:text-white">
                   Trusted marketplace
                 </h3>
                 <p className="services__content-text section-text pb-0">
@@ -40,14 +34,12 @@ export default function Services() {
             </div>
 
             <div className="services__content flex items-start gap-6">
-              <img
-                src={ServicesIcon2}
-                alt="service icon"
-                className="services__content-icon w-10"
-              />
+              <h3 className="services__content-number font-serif text-[28px] text-gray-900 dark:text-white">
+                02
+              </h3>
 
               <div>
-                <h3 className="services__content-title pb-4 font-serif text-[20px] text-gray-900">
+                <h3 className="services__content-title pb-4 font-serif text-[20px] text-gray-900 dark:text-white">
                   Customer Gift vouchers
                 </h3>
                 <p className="services__content-text section-text pb-0">
@@ -58,14 +50,12 @@ export default function Services() {
             </div>
 
             <div className="services__content flex items-start gap-6">
-              <img
-                src={ServicesIcon3}
-                alt="service icon"
-                className="services__content-icon w-10"
-              />
+              <h3 className="services__content-number font-serif text-[28px] text-gray-900 dark:text-white">
+                03
+              </h3>
 
               <div>
-                <h3 className="services__content-title pb-4 font-serif text-[20px] text-gray-900">
+                <h3 className="services__content-title pb-4 font-serif text-[20px] text-gray-900 dark:text-white">
                   Multiple Awards wins
                 </h3>
                 <p className="services__content-text section-text pb-0">
