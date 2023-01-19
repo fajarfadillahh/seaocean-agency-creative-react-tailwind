f9405a1663fa9f11bdf7ff8322567d0ce3190d83
fajarfadillahh <fajaragustian979@gmail.com>
2023-01-19 19:31:26 +0700

    fixed header shadow

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { RiMenu3Line, RiCloseLine } from "react-icons/ri";
 
@@ -14,19 +14,25 @@ export default function Header() {
 
   // add header shadow when scrolling
   const [headerShadow, setHeaderShadow] = useState(false);
-  const addHeaderShadow = () => {
-    if (window.scrollY > 10) {
-      setHeaderShadow(true);
-    } else {
-      setHeaderShadow(false);
-    }
-  };
-  window.addEventListener("scroll", addHeaderShadow);
+  useEffect(() => {
+    const addHeaderShadow = () => {
+      if (window.scrollY >= 50) {
+        setHeaderShadow(true);
+      } else if (window.scrollY <= 100) {
+        setHeaderShadow(false);
+      }
+    };
+    window.addEventListener("scroll", addHeaderShadow);
+
+    return () => {
+      window.removeEventListener("scroll", addHeaderShadow);
+    };
+  }, []);
 
   return (
     <header
       className={`header fixed top-0 left-0 z-20 w-full bg-white transition-all duration-300 ${
-        headerShadow ? "shadow-lg" : "shadow-none"
+        headerShadow ? "shadow-md" : "shadow-none"
       }`}
     >
       <div className="header__container container flex h-24 items-center justify-between">
@@ -43,7 +49,7 @@ export default function Header() {
         </Link>
 
         <div
-          className={`header__menu fixed top-24 left-0 w-full origin-top bg-white py-8 px-16 text-center shadow-lg transition-all duration-300 md:static md:top-0 md:flex md:w-max md:scale-y-100 md:items-center md:gap-8 md:p-0 md:opacity-100 md:shadow-none lg:gap-12 ${
+          className={`header__menu fixed top-24 left-0 w-full origin-top bg-white py-8 px-16 text-center shadow-md transition-all duration-300 md:static md:top-0 md:flex md:w-max md:scale-y-100 md:items-center md:gap-8 md:p-0 md:opacity-100 md:shadow-none lg:gap-12 ${
             menuOpen ? "scale-y-100 opacity-100" : "scale-y-0 opacity-0"
           }`}
         >
