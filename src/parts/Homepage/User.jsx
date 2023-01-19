0e8375b13b52296c3b49a47d20ec55b7b17b8d31
fajarfadillahh <fajaragustian979@gmail.com>
2023-01-19 23:36:32 +0700

    dark mode User

--- src/parts/Homepage/User.jsx
+++ src/parts/Homepage/User.jsx
@@ -4,7 +4,7 @@ import UserImg from "../../assets/images/user-img.png";
 
 export default function User() {
   return (
-    <section className="user section">
+    <section className="user section transition-all duration-300 dark:bg-gray-900">
       <div className="user__container container grid gap-12">
         <div className="user__data justify-self-center text-center xs:max-w-sm sm:max-w-md md:max-w-lg lg:max-w-xl xl:max-w-2xl">
           <h1 className="user__title section-title">
