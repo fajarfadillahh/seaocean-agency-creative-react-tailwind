80bf375b320f1d284074f0152ebdf135f8511169
fajarfadillahh <fajaragustian979@gmail.com>
2023-01-19 23:38:20 +0700

    dark mode Work

--- src/parts/Homepage/Work.jsx
+++ src/parts/Homepage/Work.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 export default function Work() {
   return (
-    <section className="work section">
-      <div className="work__wrapper bg-[#f9fafc] py-16">
+    <section className="work section transition-all duration-300 dark:bg-gray-900">
+      <div className="work__wrapper bg-[#f9fafc] py-16 transition-all duration-300 dark:bg-gray-800">
         <div className="work__container container grid gap-12">
           <div className="work__data justify-self-center text-center xs:max-w-sm sm:max-w-md lg:max-w-xl">
             <h1 className="work__title section-title">
@@ -18,12 +18,12 @@ export default function Work() {
 
           <div className="work__group grid items-center gap-8 justify-self-center xs:max-w-sm sm:max-w-md lg:max-w-3xl lg:grid-cols-2 xl:max-w-5xl xl:grid-cols-3">
             <div className="work__content flex items-start gap-6">
-              <h3 className="work__number font-serif text-[28px] text-gray-900">
+              <h3 className="work__content-number font-serif text-[28px] text-gray-900 dark:text-white">
                 01
               </h3>
 
               <div>
-                <h3 className="work__content-title pb-4 font-serif text-[20px] text-gray-900">
+                <h3 className="work__content-title pb-4 font-serif text-[20px] text-gray-900 dark:text-white">
                   Connect with support
                 </h3>
                 <p className="work__content-text section-text pb-0">
@@ -34,12 +34,12 @@ export default function Work() {
             </div>
 
             <div className="work__content flex items-start gap-6">
-              <h3 className="work__number font-serif text-[28px] text-gray-900">
+              <h3 className="work__content-number font-serif text-[28px] text-gray-900 dark:text-white">
                 02
               </h3>
 
               <div>
-                <h3 className="work__content-title pb-4 font-serif text-[20px] text-gray-900">
+                <h3 className="work__content-title pb-4 font-serif text-[20px] text-gray-900 dark:text-white">
                   Explain your business idea
                 </h3>
                 <p className="work__content-text section-text pb-0">
@@ -50,12 +50,12 @@ export default function Work() {
             </div>
 
             <div className="work__content flex items-start gap-6">
-              <h3 className="work__number font-serif text-[28px] text-gray-900">
+              <h3 className="work__content-number font-serif text-[28px] text-gray-900 dark:text-white">
                 03
               </h3>
 
               <div>
-                <h3 className="work__content-title pb-4 font-serif text-[20px] text-gray-900">
+                <h3 className="work__content-title pb-4 font-serif text-[20px] text-gray-900 dark:text-white">
                   Be partner & earn money
                 </h3>
                 <p className="work__content-text section-text pb-0">
