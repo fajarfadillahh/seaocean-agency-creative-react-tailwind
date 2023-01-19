91e8520b96441931527256c8d7fec01fa826483f
fajarfadillahh <fajaragustian979@gmail.com>
2023-01-19 23:11:19 +0700

    create custom hooks useLocalStorage & useColorMode for dark mode feature

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,21 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import { RiMenu3Line, RiCloseLine } from "react-icons/ri";
+import { RiMenu3Line, RiCloseLine, RiMoonFill } from "react-icons/ri";
+
+import useColorMode from "../hooks/useColorMode";
 
 import HeaderIcon from "../assets/images/icon.svg";
 import Button from "./Button";
 
 export default function Header() {
-  // header toggle
+  // === header toggle ===
   const [menuOpen, setMenuOpen] = useState(false);
 
   const setHandleMenu = () => {
     setMenuOpen((even) => !even);
   };
 
-  // add header shadow when scrolling
+  // === add header shadow when scrolling ===
   const [headerShadow, setHeaderShadow] = useState(false);
 
   useEffect(() => {
@@ -27,16 +29,19 @@ export default function Header() {
     };
   }, []);
 
+  // === dark mode ===
+  const [colorMode, setColorMode] = useColorMode();
+
   return (
     <header
-      className={`header fixed top-0 left-0 z-20 w-full bg-white transition-all duration-300 ${
+      className={`header fixed top-0 left-0 z-20 w-full bg-white transition-all duration-300 dark:bg-gray-900 ${
         headerShadow ? "shadow-md" : "shadow-none"
       }`}
     >
       <div className="header__container container flex h-24 items-center justify-between">
         <Link
           to="/"
-          className="header__link inline-flex items-center font-serif text-[20px] text-gray-900"
+          className="header__link inline-flex items-center font-serif text-[20px] text-gray-900 dark:text-white"
         >
           <img
             src={HeaderIcon}
@@ -47,7 +52,7 @@ export default function Header() {
         </Link>
 
         <div
-          className={`header__menu fixed top-24 left-0 w-full origin-top bg-white py-8 px-16 text-center shadow-md transition-all duration-300 md:static md:top-0 md:flex md:w-max md:scale-y-100 md:items-center md:gap-8 md:p-0 md:opacity-100 md:shadow-none lg:gap-12 ${
+          className={`header__menu fixed top-24 left-0 w-full origin-top bg-white py-8 px-16 text-center shadow-md transition-all duration-300 dark:bg-gray-900 md:static md:top-0 md:flex md:w-max md:scale-y-100 md:items-center md:gap-6 md:p-0 md:opacity-100 md:shadow-none lg:gap-8 ${
             menuOpen ? "scale-y-100 opacity-100" : "scale-y-0 opacity-0"
           }`}
         >
@@ -61,7 +66,7 @@ export default function Header() {
               <li key={url.toString()}>
                 <Link
                   to={url}
-                  className="header__link text-[15px] font-medium text-gray-900 hover:text-red-600"
+                  className="header__link text-[15px] font-medium text-gray-900 hover:text-red-600 dark:text-white dark:hover:text-red-600"
                 >
                   {title}
                 </Link>
@@ -69,11 +74,21 @@ export default function Header() {
             ))}
           </ul>
 
+          {/* themme toggle */}
+          <div
+            className="header__theme absolute right-12 top-4 inline-flex cursor-pointer text-[1.3rem] text-gray-900 dark:text-white md:static"
+            onClick={() =>
+              setColorMode(colorMode === "light" ? "dark" : "light")
+            }
+          >
+            <RiMoonFill />
+          </div>
+
           <Button url="/">get started</Button>
         </div>
 
         <div
-          className="header__toggle inline-flex cursor-pointer text-[1.3rem] text-gray-900 md:hidden"
+          className="header__toggle inline-flex cursor-pointer text-[1.3rem] text-gray-900 dark:text-white md:hidden"
           onClick={setHandleMenu}
         >
           {!menuOpen ? <RiMenu3Line /> : <RiCloseLine />}
---
+++ src/hooks/useColorMode.jsx
@@ -0,0 +1,20 @@
+import { useEffect } from "react";
+
+import useLocalStorage from "./useLocalStorage";
+
+function useColorMode() {
+  const [colorMode, setColorMode] = useLocalStorage("color-mode", "light");
+
+  useEffect(() => {
+    const className = "dark";
+    const bodyClasses = window.document.body.classList;
+
+    colorMode === "dark"
+      ? bodyClasses.add(className)
+      : bodyClasses.remove(className);
+  }, [colorMode]);
+
+  return [colorMode, setColorMode];
+}
+
+export default useColorMode;
